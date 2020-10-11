ead84ff4ba8c588daec8fb0e132a22b9aa3e550c
Sergey0705 <pavrun85@mail.ru>
2020-10-11 16:48:33 +0600

    Решение с помощью метода reduce

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -12,7 +12,7 @@ function truncate(str, maxlength) {
     if (trimmedStr.length <= maxlength) {
       return (trimmedStr.length === 0) ? trimmedStr + partialStr : trimmedStr + ' ' + partialStr
     }
-      return trimmedStr.replace(trimmedStr[19], '…').slice(0, 20)
+      return trimmedStr.replace(trimmedStr[19], '…').slice(0, maxlength)
   }, '')
   
   return result
