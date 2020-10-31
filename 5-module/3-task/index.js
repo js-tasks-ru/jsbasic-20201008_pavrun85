d1c9e57473d18d0830b07a6622a6f5873de45cbf
Sergey0705 <pavrun85@mail.ru>
2020-10-31 16:21:47 +0600

    Решение

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
   // ваш код...
+  let container = document.querySelector('.container')
+  let elem = document.querySelector('.carousel__inner')
+  let slides = document.querySelectorAll('.carousel__slide')
+  let width = elem.offsetWidth
+  let couruselArrowLeft = document.querySelector('.carousel__arrow_left')
+  let couruselArrowRight = document.querySelector('.carousel__arrow_right')
+  
+  let position = 0
+  if (position === 0) {
+    couruselArrowLeft.style.display = 'none'
+  } else {
+    couruselArrowLeft.style.display = ''
+  }
+
+  if (slides.length - 1) {
+    couruselArrowRight.style.display = 'none'
+  } else {
+    couruselArrowRight.style.display = ''
+  }
+  
+  container.addEventListener('click', event => {
+    if (event.target.classList.contains('carousel__arrow_right')) {
+      position -= width
+      elem.style.transform = `translateX(${position}px)`
+    } else if (event.target.classList.contains('carousel__arrow_left')) {
+      position += width
+      elem.style.transform = `translateX(${position}px)`
+    }
+  })
 }
+
+
