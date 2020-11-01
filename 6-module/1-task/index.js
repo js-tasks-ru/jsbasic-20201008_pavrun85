8233e7bc60702f353cbefac038fc845c78361270
Sergey0705 <pavrun85@mail.ru>
2020-11-02 02:03:02 +0600

    Решение

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,29 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows
+    this.table = document.createElement('table')
+    // Создаю "шапку" таблицы
+    this.tHead = '<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody><tr>'
+    // Вызываю метод, чтобы создать таблицу 
+    this.elem = this['makeTable']()
+    // На созданную таблицу устанавливаю слушатель событий, чтобы при клике на кнопку удалить строку
+    this.table.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') {
+        let target = event.target.closest('tr')
+        target.remove()
+      }
+    })
+  }
+
+  makeTable() {
+    this.value = this.rows.reduce((string, user) => {
+      if (user.length - 1) {
+        return string + '<td>' + user.name + '</td>' + '<td>' + user.age + '</td>' + '<td>' + user.salary + '</td>' + '<td>' + user.city + '</td>' + '<td>' + '<button>' + 'X' + '</button>' + '</td>' + '</tr>' + '</tbody>'
+      }
+     return string + '<td>' + user.name + '</td>' + '<td>' + user.age + '</td>' + '<td>' + user.salary + '</td>' + '<td>' + user.city + '</td>' + '<td>' + '<button>' + 'X' + '</button>' + '</td>' + '</tr>'  
+    }, this.tHead)
+    this.table.innerHTML = this.value
+    return this.table
   }
 }
